ee58bb433c0a5d0202a159a3a2b7193616a3cb88
Anene Mary-Cynthia <innocentmarycynthia3@gmail.com>
2022-06-25 11:44:33 +0100

    first push to github

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstName = "Mary-Cynthia";
+const lastName = "Anene";
+let fullName = firstName + " " + lastName;
+let age = 30;
+let height = "5'3ft";
+let country = "Nigerian";
+console.log("My name is", fullName);
+console.log("I am", height, "tall");
+console.log("I am a", country);
